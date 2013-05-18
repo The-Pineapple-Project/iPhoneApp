44541d612087ee83ec7ed5eb6773ee8bc14a00c0
Andrew Ash <enderash@gmail.com>
2013-05-18 15:58:00 -0400

    empty project, for iPhone, iOS 6.1 (not a universal app)

---
+++ WhatToPlant/WhatToPlantAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WhatToPlantAppDelegate.h
+//  WhatToPlant
+//
+//  Created by aash on 2013-05-18.
+//  Copyright (c) 2013 The Pineapple Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhatToPlantAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhatToPlant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhatToPlant
+//
+//  Created by aash on 2013-05-18.
+//  Copyright (c) 2013 The Pineapple Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WhatToPlantAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WhatToPlantAppDelegate class]));
+    }
+}
